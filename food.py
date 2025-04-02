b54fdeffd87a0dc7127133cfd60842092c2654f0
pchowdary16 <praneethayadlapalli06@gmail.com>
2025-04-02 16:51:04 +0530

    Update food.py

--- food.py
+++ food.py
@@ -1,11 +1,5 @@
 import streamlit as st
-import random
-import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import StandardScaler
+
 
 def calculate_bmi(weight, height):
     if height > 0:
